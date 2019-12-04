8460ba6b9ca714194bf99a7654912b1d90a94e89
Herrada <juanfernandoherrada13@gmail.com>
2019-12-04 11:03:51 +0100

    File repertory

--- src/main/java/com/chanoir/imagefilter/Main.java
+++ src/main/java/com/chanoir/imagefilter/Main.java
@@ -1,9 +1,13 @@
 package com.chanoir.imagefilter;
 
+import org.bytedeco.opencv.opencv_core.Mat;
+import java.io.File;
+
 public class Main {
+
+    public static File repertory = new File("ImageFile.java");
+
     public static void main(String[] args) {
-
         
-
     }
 }
