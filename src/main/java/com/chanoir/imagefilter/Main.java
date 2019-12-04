170a36041f20f58fdddc0aab23c875b0de59ccf7
Alyssia <alyssia0207@gmail.com>
2019-12-04 10:11:49 +0100

    filters

--- src/main/java/com/chanoir/imagefilter/Main.java
+++ src/main/java/com/chanoir/imagefilter/Main.java
@@ -1,9 +1,10 @@
 package com.chanoir.imagefilter;
 
+import org.opencv.core.Mat;
+import org.opencv.imgcodecs.Imgcodecs;
+
 public class Main {
     public static void main(String[] args) {
-
-        
 
     }
 }
